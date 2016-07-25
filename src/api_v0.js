cf8f1f18d6c70cac73f59d34c00642093cf7d991
Sam H <sam@email.ly>
2016-07-25 17:34:42 -0400

    Update API proxy endpoint

--- src/api_v0.js
+++ src/api_v0.js
@@ -3,5 +3,5 @@ const proxy = require('express-request-proxy');
 module.exports = proxy({
   cache: false,
   timeout: 3000,
-  url: `https://budtheory.webjoint.com/*`,
+  url: `http://budtheory.webjoint.com/*`,
 });
