c1ac20134b8d5b8a75af4dd208565f7d2b257bfd
Sam H <sam@email.ly>
2016-07-27 07:17:46 -0400

    Raise proxy timeout to 10sec

--- src/api_v0.js
+++ src/api_v0.js
@@ -2,7 +2,7 @@ const proxy = require('express-request-proxy');
 
 module.exports = (options) => proxy({
   cache: false,
-  timeout: 3000,
+  timeout: 10000,
   url: `https://198.199.100.16/*`,
   spoofCookies: options.spoofCookies,
   headers: {
